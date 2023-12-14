9605b78878f5a40c5a88eac159f62a7f8401990d
Fahmida Faiza <fahmidafaiza57@gmail.com>
2023-12-14 16:43:15 +0600

    skill added

--- src/components/Skill.jsx
+++ src/components/Skill.jsx
@@ -4,14 +4,18 @@ const Skill = () => {
         <div>
             <h3 className=" text-center font-extrabold text-5xl py-7"> My Skills </h3>
             <div className="grid grid-cols-4 gap-4 rounded-full">
-               <div><img src="css.png" alt="" /></div>
-               <div><img src="firebase.png" alt="" /></div>
-               <div><img src="node-js.png" alt="" /></div>
-               <div><img src="express.png" alt="" /></div>
-               <div><img src="gitBash.png" alt="" /></div>
-               <div><img src="mongodb.png" alt="" /></div>
-               <div><img src="bootstrap.png" alt="" /></div>
-               <div><img src="html.png" alt="" /></div>
+               <div><img className="w-1/2" src="css.png" alt="" /></div>
+               <div><img className="w-1/2" src="firebase.png" alt="" /></div>
+               <div><img className="w-1/2" src="node-js.png" alt="" /></div>
+               <div><img className="w-1/2" src="express.png" alt="" /></div>
+               <div><img className="w-1/2" src="gitBash.png" alt="" /></div>
+               <div><img className="w-1/2" src="mongodb.png" alt="" /></div>
+               <div><img className="w-1/2" src="bootstrap.png" alt="" /></div>
+               <div><img className="w-1/2" src="react.png" alt="" /></div>
+               <div><img className="w-2/3" src="js.png" alt="" /></div>
+                <div><img className="w-2/3" src="tailwind.png" alt="" /></div>
+               <div><img className="" src="daisyui1.png" alt="" /></div>
+               <div><img className="w-2/3" src="html.png" alt="" /></div>
               
             </div>
         </div>
